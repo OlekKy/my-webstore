56d418e2665ac837bf4f99568e70a6d237d5141f
OlekKy <a1795k@gmail.com>
2018-02-25 17:33:06 +0100

    Rewizja 6. Dodanie wyswietlania produktow na podstawie kategorii. Dodanie wyswietlania produktow na podstawie zmiennych tablicowych. Dodanie mozliwosci wyswietlania produktu na podstawie podanego id produktu. Implementacja widoku strony typu master-detail.

--- src/main/java/com/packt/webstore/service/impl/ProductServiceImpl.java
+++ src/main/java/com/packt/webstore/service/impl/ProductServiceImpl.java
@@ -1,6 +1,9 @@
 package com.packt.webstore.service.impl;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.packt.webstore.domain.Product;
@@ -25,4 +28,7 @@ public class ProductServiceImpl implements ProductService {
 		return productRepository.getProductsByCategory(category);
 	}
 	
+	public Set<Product> getProductsByFilter(Map<String, List<String>> filterParams){
+		return productRepository.getProductsByFilter(filterParams);
+	}
 }
